e59b15c02f5906177334e17829191dc21888b122
PRIYANKA <PRIYANKA@LAPTOP-JH1AORIL>
2025-09-16 14:41:35 +0530

    commit extra

--- AutomationCourse/src/test/java/seleniumBasics/Locators.java
+++ AutomationCourse/src/test/java/seleniumBasics/Locators.java
@@ -13,6 +13,23 @@ public class Locators extends Base {
 		driver.findElement(By.partialLinkText("Simple"));
 		driver.findElement(By.cssSelector("button[id='button-one']"));
 		
+		// Absolute xpath : /html/body/section/div/div/div[2]/div[1]/div/div[2]/form/button
+		
+		driver.findElement(By.xpath("//button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[text()='Show Message']"));
+		driver.findElement(By.xpath("//button[starts-with(text(),'Show ')]"));
+		driver.findElement(By.xpath("//button[@id='button-one'and @type='button']"));
+		driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));	
+		driver.findElement(By.xpath("//div[contains (text(),'Single Input Field')]//parent::div[@class='card']"));
+		driver.findElement(By.xpath("//div[@class='card']//child::button[@id='button-one']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//following::div[@class='card']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//preceding:: div[@class='card']"));
+		driver.findElement(By.xpath("//button[@id='button-one']//ancestor::div"));
+		driver.findElement(By.xpath("//div[@class='card']//descendant::div"));
+		
+		
+		
+		
 	}
 	
 	
