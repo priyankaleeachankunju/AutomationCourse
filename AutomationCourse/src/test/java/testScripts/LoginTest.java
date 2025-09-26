7898f86042db2ee42714b2e3a2f5c83a2315309a
PRIYANKA <PRIYANKA@LAPTOP-JH1AORIL>
2025-09-26 21:04:21 +0530

    pushing2

--- AutomationCourse/src/test/java/testScripts/LoginTest.java
+++ AutomationCourse/src/test/java/testScripts/LoginTest.java
@@ -5,17 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import automationCore.TestNGBase;
+import pages.LoginPage;
 
 public class LoginTest extends TestNGBase {
 	
 	@Test
 	public void verifyWetherUserIsAbleToLoginWithValidCredentials() {
-		WebElement Username = driver.findElement(By.id("user-name"));
-		Username.sendKeys("standard_user");
-		WebElement Password = driver.findElement(By.id("password"));
-		Password.sendKeys("secret_sauce");
-		WebElement LoginBtn = driver.findElement(By.id("login-button"));
-		LoginBtn.click();
+		
+		LoginPage loginpage = new LoginPage(driver);
+		loginpage.enterUserNameOnUserNameField();
+		loginpage.enterPasswordOnPasswordField();
+		loginpage.loginButtonClick();
+		
 	}
 
 	@Test
